b5ead4a00dc7aab89671f585f15c9b947f623ffe
Carlos Amaral <v-carlos.amaral@avenuesec.io>
2022-12-06 18:49:46 -0300

    feat/adding services

--- src/services/event-service.jsx
+++ src/services/event-service.jsx
@@ -2,57 +2,71 @@ import useDebounce from "../utils/debounce";
 import { doGet, doAwaitGet, doPost } from "../utils/http";
 import TokenService from "./token-service";
 
-// TODO: Em todos os métodos deve ser validado se o body está correto
 export default class EventService {
   constructor() {
     this.authPath = process.env.REACT_APP_BACKEND_AUTH_PATH;
     this.userPath = process.env.REACT_APP_BACKEND_USER_PATH;
     this.eventPath = process.env.REACT_APP_BACKEND_EVENT_PATH;
+
+    this.events = [];
+
     this.tokenService = new TokenService();
   }
 
-  async all() {
+  async getAll() {
     let res = await doAwaitGet(this.eventPath, null);
+    this.events = res.data;
+    return this.events;
+  }
+
+  async getById(id) {
+    if (id === null) {
+      id = this.getIdFromPath();
+    }
+
+    let res = await doAwaitGet(
+      `${process.env.REACT_APP_BACK_URL_EVENTS_BY_ID}/${id}`,
+      null
+    );
+
     return res.data;
   }
 
-  byId(id) {
-    const url = `${process.env.REACT_APP_BACK_URL_EVENTS_BY_ID}/${id}`;
-    return doAwaitGet(url, null);
-  }
+  async getByName(name) {
+    let eventsToReturn = [];
 
-  byName(name) {
-    const url = `${process.env.REACT_APP_BACK_URL_EVENTS_BY_NAME}/${name}`;
-    return doAwaitGet(url, null);
-  }
+    if (this.events.length === 0) {
+      await this.getAll();
+    }
 
-  byMusicalType(musicalType) {
-    const url = `${process.env.REACT_APP_BACK_URL_EVENTS_BY_MUSICAL_TYPE}/${musicalType}`;
-    return doAwaitGet(url, null);
-  }
+    this.events.forEach((event) => {
+      if (event.name.toUpperCase().includes(name.toUpperCase())) {
+        eventsToReturn.push(event);
+      }
+    });
 
-  byAge(age) {
-    const url = `${process.env.REACT_APP_BACK_URL_EVENTS_BY_AGE}/${age}`;
-    return doAwaitGet(url, null);
-  }
-
-  byCity(city) {
-    const url = `${process.env.REACT_APP_BACK_URL_EVENTS_BY_CITY}/${city}`;
-    return doAwaitGet(url, null);
+    return eventsToReturn;
   }
 
   buy(body) {
     return doPost(process.env.REACT_APP_BACK_URL_EVENTS_BUY, body, null);
   }
 
-  sell(body) {
-    return doPost(process.env.REACT_APP_BACK_URL_EVENTS_SELL, body, null);
-  }
+  // sell(body) {
+  //   return doPost(process.env.REACT_APP_BACK_URL_EVENTS_SELL, body, null);
+  // }
 
   share() {
     // Sugestão: enviar os dados para um dashboard para ser uma métrica
     navigator.clipboard.writeText(window.location.href);
   }
-}
 
-const eventService = new EventService();
+  getIdFromPath() {
+    const url = window.location.href;
+    const word = "tests/";
+    const index = url.indexOf(word);
+    const eventID = url.slice(index + word.length, url.length).toString();
+
+    console.log(eventID);
+  }
+}
--- src/utils/controller.jsx
+++ src/utils/controller.jsx
@@ -1,13 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "../app/pages/home/home-page";
-import IntegrationPage from "../app/pages/integration-test/integration-test-page";
 
-export const Controller = (props) => {
+export const Controller = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path={"/"} element={<HomePage />} />
-        <Route path={"/tests"} element={<IntegrationPage />} />
       </Routes>
     </BrowserRouter>
   );
