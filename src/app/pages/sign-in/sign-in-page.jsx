5a112d69b9e9d3ab5c25669633f4bf3dcc5984cf
Igor Rosiak <86728719+Igorrosiak@users.noreply.github.com>
2022-12-06 01:10:31 -0300

    updating/finished screen sketch

---
+++ src/app/pages/sign-in/sign-in-page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { SignInFormWidget } from "../../widgets/sign-in-form/sign-in-form-widget"
+import './sign-in-page.scss'
+
+export default class SignInPage extends React.Component {
+
+  render() {
+    return (
+      <div className="m-auto sign-in-page">
+        <SignInFormWidget/>
+      </div>
+    )
+  }
+}
--- src/utils/controller.js
+++ src/utils/controller.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import HomePage from "../app/pages/home/home-page";
+import SignInPage from "../app/pages/sign-in/sign-in-page";
 
 export const Controller = (props) => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path={"/"} element={<HomePage />} />
+        <Route path={"/sign-in"} element={<SignInPage />} />
       </Routes>
     </BrowserRouter>
   );
