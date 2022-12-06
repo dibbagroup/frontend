2135e6e9dae3c770a47aafca0418d01edec654fa
Igor Rosiak <86728719+Igorrosiak@users.noreply.github.com>
2022-12-06 01:10:31 -0300

    updating/finished screen sketch

--- src/utils/controller.jsx
+++ src/utils/controller.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "../app/pages/home/home-page";
+import SignInPage from "../app/pages/sign-in/sign-in-page";
 
 export const Controller = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path={"/"} element={<HomePage />} />
+        <Route path={"/sign-in"} element={<SignInPage />} />
       </Routes>
     </BrowserRouter>
   );
