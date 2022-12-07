4c26742179cd3e77eadcfdca82d3499d1f64e5a0
Carlos Amaral <v-carlos.amaral@avenuesec.io>
2022-12-06 21:47:13 -0300

    resolving conflits

--- src/utils/controller.jsx
+++ src/utils/controller.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "../app/pages/home/home-page";
 import SignInPage from "../app/pages/sign-in/sign-in-page";
 import SignUpPage from "../app/pages/sign-up/sign-up-page";
+import SignInPage from "../app/pages/sign-in/sign-in-page";
 
 export const Controller = () => {
   return (
@@ -10,6 +11,7 @@ export const Controller = () => {
         <Route path={"/"} element={<HomePage />} />
         <Route path={"/sign-in"} element={<SignInPage />} />
         <Route path={"/sign-up"} element={<SignUpPage />} />
+        <Route path={"/sign-in"} element={<SignInPage />} />
       </Routes>
     </BrowserRouter>
   );
