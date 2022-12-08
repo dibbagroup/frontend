55552555e246b02a8c7dd3b5a6ef5169ff93385f
Igor Rosiak <86728719+Igorrosiak@users.noreply.github.com>
2022-12-07 23:54:52 -0300

    updating/new background

--- src/app/widgets/sign-in-form/sign-in-form-widget.jsx
+++ src/app/widgets/sign-in-form/sign-in-form-widget.jsx
@@ -1,14 +1,18 @@
 import { Form, Button } from 'react-bootstrap';
 import './sign-in-form-widget.scss'
+import logoDibba from '../../../assets/images/logo_login.png'
 
 export const SignInFormWidget = props => {
     return (
         <Form>
+            <div className="logo mb-4">
+                <img src={logoDibba} alt="Logo Dibba" />
+            </div>
             <Form.Text className="greeting">
-                Olá
+                Seja bem-vindo!
             </Form.Text>
             <Form.Text className="mb-5 motivate">
-                Faça seu login para continuar
+                Faça seu login para continuar.
             </Form.Text>
             <Form.Control className="form-input mb-3" type="email" placeholder="Email ou CNPJ da empresa" />
             <Form.Control className="form-input mb-3" type="password" placeholder="Senha" />
