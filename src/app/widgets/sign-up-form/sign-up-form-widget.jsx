5adeb6d1262dca6025df187346e6a03866f279cc
Igor Rosiak <86728719+Igorrosiak@users.noreply.github.com>
2022-12-10 02:48:19 -0300

    updating/finished screen

--- src/app/widgets/sign-up-form/sign-up-form-widget.jsx
+++ src/app/widgets/sign-up-form/sign-up-form-widget.jsx
@@ -7,6 +7,33 @@ export const SignUpFormWidget = props => {
 
     const [inputDateType, setInputDateType] = useState("text")
 
+    const [passwordSee, setPasswordSee] = useState(false)
+    const [confirmPasswordSee, setConfirmPasswordSee] = useState(false)
+    const [passwordSeeType, setPasswordSeeType] = useState("password")
+    const [confirmPasswordSeeType, setConfirmPasswordSeeType] = useState("password")
+
+    function verSenha(){
+        if(passwordSee === false){
+            console.log("viu senha")
+            setPasswordSee(true)            
+            setPasswordSeeType("text")
+        } else{
+            console.log("deixou de ver senha")
+            setPasswordSee(false)
+            setPasswordSeeType("password")
+        }
+    }
+
+    function verConfirmarSenha(){
+        if(confirmPasswordSee === false){
+            setConfirmPasswordSee(true)            
+            setConfirmPasswordSeeType("text")
+        } else{
+            setConfirmPasswordSee(false)
+            setConfirmPasswordSeeType("password")
+        }
+    }
+
     return (
         <Form className="sign-up-form">
             <div className="logo mb-3">
@@ -21,10 +48,10 @@ export const SignUpFormWidget = props => {
             <Form.Control type="email" className="form-input mb-2" placeholder="Email" />
             <Form.Control type="text" className="form-input mb-2" placeholder="CPF" />
             <Form.Control type={inputDateType} className="form-input mb-2" placeholder="Data de Nascimento" onFocus={()=> {setInputDateType("date")}} />
-            <div className="eye-password"><i className="fi fi-rs-eye"></i></div>
-            <Form.Control type="password" className="form-input mb-2" id="password" placeholder="Senha" />
-            <div className="eye-password"><i className="fi fi-rs-eye"></i></div>
-            <Form.Control type="password" className="form-input mb-2" id="password" placeholder="Confirme sua Senha" />
+            <div className="eye-password"><i className="fi fi-rs-eye" onClick={()=> {verSenha()}}></i></div>
+            <Form.Control type={passwordSeeType} className="form-input mb-2" placeholder="Senha" />
+            <div className="eye-password"><i className="fi fi-rs-eye" onClick={()=> {verConfirmarSenha()}}></i></div>
+            <Form.Control type={confirmPasswordSeeType} className="form-input mb-2" placeholder="Confirme sua Senha" />
             <Form.Group className="terms">
                 <Form.Check type="checkbox" className="check" />
                 <Form.Text className="terms-text">Concordo com os termos de privacidade</Form.Text>
