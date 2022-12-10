468ba565e19f7856febf936c15a99f96c827d8b7
Carlos Amaral <v-carlos.amaral@avenuesec.io>
2022-12-10 09:33:10 -0300

    fix/cleaning code

--- src/app/pages/home/home-page.jsx
+++ src/app/pages/home/home-page.jsx
@@ -5,7 +5,7 @@ export default class HomePage extends React.Component {
   constructor() {
     super();
     this.state = {
-      title: "Title Teste Deploy",
+      title: "Title Teste",
     };
   }
 
