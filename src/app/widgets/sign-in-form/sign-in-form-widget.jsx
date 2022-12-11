2ba3072d57dbba1a993a044b72b1a93afa6bd926
Igor Rosiak <86728719+Igorrosiak@users.noreply.github.com>
2022-12-10 22:53:50 -0300

    integration finished

--- src/app/widgets/sign-in-form/sign-in-form-widget.jsx
+++ src/app/widgets/sign-in-form/sign-in-form-widget.jsx
@@ -2,8 +2,14 @@ import { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import './sign-in-form-widget.scss'
 import logoDibba from '../../../assets/images/logo_login.png'
+import AuthService from '../../../services/auth-service';
 
 export const SignInFormWidget = props => {
+
+    const authenticationService = new AuthService()
+
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
 
     const [showPassword, setShowPassword] = useState(false)
     const [showPasswordType, setShowPasswordType] = useState("password")
@@ -29,13 +35,13 @@ export const SignInFormWidget = props => {
             <Form.Text className="mb-5 subtitle">
                 Faça seu login para continuar.
             </Form.Text>
-            <Form.Control className="form-input mb-3" type="email" placeholder="Email ou CNPJ da empresa" />
+            <Form.Control className="form-input mb-3" type="email" placeholder="Email" onChange={(e)=> setUsername(e.target.value)} />
             <div className="eye-password"><i className="fi fi-rs-eye" onClick={()=> {handleShowPassword()}}></i></div>
-            <Form.Control type={showPasswordType} className="form-input mb-3" placeholder="Senha" />
+            <Form.Control type={showPasswordType} className="form-input mb-3" placeholder="Senha" onChange={(e)=> setPassword(e.target.value)} />
             <Form.Text className="recover-password mb-3">
                 <a href="/">Esqueceu a senha?</a>
             </Form.Text>
-            <Button className="mb-2 login" type="submit">
+            <Button className="mb-2 login" type="submit" onClick={()=> {authenticationService.signIn(username, password)}}>
                 Entrar
             </Button>
             <div>
