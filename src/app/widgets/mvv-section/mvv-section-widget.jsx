69c212113c4e0d4b0a4d11e569d55c9dc27d1504
Carlos Amaral <v-carlos.amaral@avenuesec.io>
2022-12-11 18:54:45 -0300

    feat/creating page and widgets

--- src/app/widgets/mvv-section/mvv-section-widget.jsx
+++ src/app/widgets/mvv-section/mvv-section-widget.jsx
@@ -1,7 +1,7 @@
 export const MvvSectionWidget = (props) => {
   return (
     <section>
-      <h1>{props.title}</h1>
+      <h3>{props.title}</h3>
       <hr />
       {props.subSections.map((row, e) => (
         <div className="sub-section py-3">
